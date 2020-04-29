a42c3dd428bc40793c2f8d43d417f13e16339c60
esevre <esevre@gmail.com>
2020-04-29 18:43:35 +0900

    Added stub for file write tests. needs update later.

---
+++ Google_test/ES/file/write_test.cpp
@@ -0,0 +1,17 @@
+//
+// Created by esevre on 4/29/2020.
+//
+//  Google test C++ file for ES/file/write.hpp
+
+//
+// Do to file creation, I am not sure how to test this yet...
+
+#include <gtest/gtest.h>
+
+TEST(WriteTest01, empty_input)
+{
+    // todo: replace with real test later
+    int x = 1;
+    EXPECT_EQ(x, 1);
+}
+
